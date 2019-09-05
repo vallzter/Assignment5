c4052095207b1d79861a13b12941e96ce1cbb388
Valgeir Ingi <valgeiringi@Valgeirs-MacBook-Pro-2.local>
2019-09-05 09:54:18 +0000

    koði settur inn

--- max_int.py
+++ max_int.py
@@ -2,7 +2,19 @@
 #Notandi slær inn tölu
 #Forritið keyrir þangað til slegin er negative tala
 #Nota max til að finna hæstu integer töluna
+#Þegar neikvæð tala er slegin inn prenta max_int
 
 
 
+
+# Fill in the missing code
+max_int = 0
+while True:   
+    num_int = int(input("Input a number: "))    # Do not change this line
+    if num_int > max_int:
+        max_int = num_int
+    if num_int < 0:
+        break
+        
+print("The maximum is", max_int)    # Do not change this line
 
